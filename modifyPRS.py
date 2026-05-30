a868080caabe6d70eced544d33568cbbbae8b604
Thomasson <turodoras@gmail.com>
2026-05-30 17:49:27 +0200

    Add out split

--- modifyPRS.py
+++ modifyPRS.py
@@ -14,19 +14,30 @@ taille d'un channel In = 140 bytes
 basePRS = "./test.prs"
 newPRS = "./newPRS.prs"
 
+N_IN = 4
+N_OUT = 8
+
 IN_PAD = 0x1E
 IN_SIZE = 140
-
+OUT_PAD = 0x24E
+OUT_SIZE = 104
 
 with open(basePRS, "rb") as fin:
     baseData = fin.read()
     newData = list(baseData)
 
-    inA = newData[IN_PAD + IN_SIZE * 0 : IN_PAD + IN_SIZE * 1]
-    inB = newData[IN_PAD + IN_SIZE * 1 : IN_PAD + IN_SIZE * 2]
-    inC = newData[IN_PAD + IN_SIZE * 2 : IN_PAD + IN_SIZE * 3]
-    inD = newData[IN_PAD + IN_SIZE * 3 : IN_PAD + IN_SIZE * 4]
+    ins = [
+        newData[IN_PAD + IN_SIZE * i : IN_PAD + IN_SIZE * (i + 1)] for i in range(N_IN)
+    ]
+    outs = [
+        newData[OUT_PAD + OUT_SIZE * i : OUT_PAD + OUT_SIZE * (i + 1)]
+        for i in range(N_OUT)
+    ]
 
     for i in range(IN_SIZE):
-        if inA[i] != inB[i]:
-            print(f"{i=}, {inA[i]=}, {inB[i]=}")
+        if ins[0][i] != ins[1][i]:
+            print(f"{i=}, {ins[0][i]=}, {ins[1][i]=}")
+
+    for i in range(OUT_SIZE):
+        if outs[0][i] != outs[1][i]:
+            print(f"{i=}, {outs[0][i]=}, {outs[1][i]=}")
